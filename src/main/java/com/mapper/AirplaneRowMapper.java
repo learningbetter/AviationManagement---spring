c833909d94261cceb733649612a5eb2af62dbb48
LongZongXian <18885090900@163.com>
2023-06-28 14:44:45 +0800

    Add AirplaneRowMapper
    增加一个映射类，用于映射飞机类和数据库飞机表

---
+++ src/main/java/com/mapper/AirplaneRowMapper.java
@@ -0,0 +1,22 @@
+package com.mapper;
+
+import com.entity.Airplane;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AirplaneRowMapper implements RowMapper<Airplane> {
+
+
+	@Override
+	public Airplane mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Airplane airplane = new Airplane();
+		airplane.setAirplaneId(rs.getInt("id"));
+		airplane.setAirplaneType(rs.getString("airplaneType"));
+		airplane.setFirstClassLimit(rs.getInt("firstClassLimit"));
+		airplane.setBusinessClassLimit(rs.getInt("businessClassLimit"));
+		airplane.setEconomyClassLimit(rs.getInt("economyClassLimit"));
+		return airplane;
+	}
+}
